807c4aac0325fbf1cb1276615fab87b6bf50e7ab
goodmushroomstudio <exotarium@mail.ru>
2017-08-24 21:33:35 +0200

    upd

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class GameData {
     public static GameData gd = new GameData();
     public float f_axisX, f_axisY; // в Joystick
+    public float f_hp;
+    public float f_sp;
     public GameData()
     {
         f_axisX = 0;
