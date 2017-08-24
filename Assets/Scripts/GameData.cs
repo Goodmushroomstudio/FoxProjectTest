fb8aa957a43f2de8b21a85149d3016fcc5d38919
goodmushroomstudio <exotarium@mail.ru>
2017-08-24 21:40:46 +0200

    upd

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public class GameData {
     public static GameData gd = new GameData();
     public float f_axisX, f_axisY; // в Joystick
+    public bool b_onGround, b_onTurn; // в Control
     public GameData()
     {
         f_axisX = 0;
         f_axisY = 0;
+        b_onGround = false;
+        b_onTurn = false;
     }
 }
