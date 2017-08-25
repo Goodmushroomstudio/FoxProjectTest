d8fa1a17e03bc3b3c64ca0cd884d3a00cd305a33
goodmushroomstudio <exotarium@mail.ru>
2017-08-25 17:51:18 +0200

    v

---
+++ Assets/Scripts/Functions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Addhp ()
+    {
+        GameData.gd.f_currenthp += 10;
+    }
+    public void damage()
+    {
+        GameData.gd.f_currenthp -= 10;
+    }
+}
