c08cdf7ab0d72b818b0c9e7da5a268c2f0989c25
goodmushroomstudio <exotarium@mail.ru>
2017-08-27 13:36:18 +0200

    k

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class LevelGeneration : MonoBehaviour {
     public GameObject room;
     public int i_roomcount;
+    public GameObject[] players;
 
 	// Use this for initialization
 	void Start () {
         GameData.gd.i_roomCount = i_roomcount;
+        Instantiate(players[GameData.gd.i_currentChar], Vector3.zero, Quaternion.identity);
         for (int i = 0; i<i_roomcount;i++)
         {
             GameObject newroom = Instantiate(room, new Vector3(i*100,0,0), Quaternion.identity, this.transform);
